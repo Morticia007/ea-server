234396e07db044c4b50c7c5efcef13f037349076
Jacquelyn Elder <jacquelyn07@mac.com>
2021-05-05 14:20:18 -0500

    Remove comments

--- routes/users.js
+++ routes/users.js
@@ -8,44 +8,6 @@ const cors = require('./cors');
 
 const authMiddleware = [authenticate.verifyUser, authenticate.verifyAdmin];
 
-// router.get('/', async (req, res, next) => {
-//   // currently unused
-//   try {
-//     res.send({
-//       status: 'success',
-//       message: 'Successfully fetched list of users',
-//       data: [
-//         {
-//           userName: 'libersolis',
-//           email: 'manny.hagman@gmail.com',
-//           firstName: 'Manny',
-//           lastName: 'Hagman',
-//           avatar: '',
-//           companyName: 'Devwelopment',
-//         },
-//         {
-//           userName: 'MRSlibersolis',
-//           email: 'jacquelyn.hagman@gmail.com',
-//           firstName: 'Jacquelyn',
-//           lastName: 'Hagman',
-//           avatar: '',
-//           companyName: 'Devwelopment',
-//         },
-//         {
-//           userName: 'Kyra007',
-//           email: 'agave90@gmail.com',
-//           firstName: 'Kyra',
-//           lastName: 'Elder',
-//           avatar: '',
-//           companyName: 'Devwelopment',
-//         },
-//       ],
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
 router.get('/', cors.corsWithOptions, authMiddleware, (req, res, next) => {
   User.find()
     .then((users) => {
@@ -93,55 +55,26 @@ router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   );
 });
 
-// router.post(
-//   '/login',
-//   cors.corsWithOptions,
-//   passport.authenticate('local'),
-//   (req, res) => {
-//     console.log('logging in');
-//     console.log({
-//       body: req.body,
-//     });
-//     const token = authenticate.getToken({ _id: req.user._id });
-//     console.log('token', token);
-//     res.statusCode = 200;
-//     res.setHeader('Content-Type', 'application/json');
-//     console.log({ line: '104', token });
-//     res.json({
-//       success: true,
-//       token: token,
-//       status: 'You are successfully logged in!',
-//     });
-//   },
-// );
-
-// router.post('/login', (req, res, next) => {
-//   // currently unused
-//   const { userName } = req.body;
-
-//   console.log({
-//     status: 'Login Sucessful',
-//     message: `Welcome to Elder Apothecary '${userName}'.`,
-//   });
-//   res.status(200).send({
-//     status: 'Login Sucessful',
-//     message: `Welcome to Elder Apothecary '${userName}'.`,
-//   });
-// });
-cors.corsWithOptions,
+router.post(
+  '/login',
+  cors.corsWithOptions,
   passport.authenticate('local'),
-  router.post(
-    '/login',
-    passport.authenticate('local'),
-    async (req, res, next) => {
-      try {
-        res.send({
-          message: 'successfully logged in',
-        });
-      } catch (error) {
-        res.send('Error logging in to /login');
-      }
-    },
-  );
+  (req, res) => {
+    console.log('logging in');
+    console.log({
+      body: req.body,
+    });
+    const token = authenticate.getToken({ _id: req.user._id });
+    console.log('token', token);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    console.log({ line: '104', token });
+    res.json({
+      success: true,
+      token: token,
+      status: 'You are successfully logged in!',
+    });
+  },
+);
 
 module.exports = router;
